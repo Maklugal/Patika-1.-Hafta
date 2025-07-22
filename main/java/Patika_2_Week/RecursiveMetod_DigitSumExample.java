127af65854766bc8592ed3a7fff612621a086a2c
Mehmet Aşar Kıribiş <mehmet_asar_@hotmail.com>
2025-07-22 14:08:46 +0300

    Patika 2. Hafta

---
+++ main/java/Patika_2_Week/RecursiveMetod_DigitSumExample.java
@@ -0,0 +1,23 @@
+package Patika_2_Week;
+
+import java.util.Scanner;
+
+public class RecursiveMetod_DigitSumExample {
+    public static void main(String[] args) {
+
+        // 123 -> 1+2+3 = 6
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz: ");
+        int number = scanner.nextInt();
+
+        int sum = sumofdigits(number);
+        System.out.println(number + "basamak toplamı = " + sum);
+    }
+
+    private static int sumofdigits(int number) {
+        if (number == 0) return 0;
+        return (number % 10) + sumofdigits(number / 10);
+    }
+
+}
