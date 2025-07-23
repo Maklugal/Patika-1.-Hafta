1802cfe4fe7c276d2436f49e7d3335c5d3f7643b
Mehmet Aşar Kıribiş <mehmet_asar_@hotmail.com>
2025-07-23 16:08:03 +0300

    Patika 2. Hafta

--- main/java/Patika_2_Week/Employee.java
+++ main/java/Patika_2_Week/Employee.java
@@ -1,11 +1,26 @@
 package Patika_2_Week;
 
-public class Employee {
+class Main {
+    public static void main(String[] args) {
+
+        // Bir çalışan (Employee) nesnesi oluşturuyoruz.
+        Employee emp1 = new Employee("Kemal", 2000.0, 45, 1985);
+
+        // Çaşışan bilgilieri ve hesaplanan maaş detaylarını yazdırıyoruz.
+        System.out.println(emp1);
+    }
+}
+
+// Employee sınıfı: Çalışan bilgilerini tutar ve maaş hesaplamaları yapar.
+class Employee {
+
+    // Nitelikler
     String name;
     double salary;
     int workHours;
     int hireYear;
 
+    // Constructor : Nesne oluştururken ilk değerler atanır.
     public Employee(String name, double salary, int workHours, int hireYear) {
         this.name = name;
         this.salary = salary;
@@ -13,27 +28,33 @@ public class Employee {
         this.hireYear = hireYear;
     }
 
+    // Maaşa vergi uygular. 1000 TL altı ise verdi yok, üstü ise %3 vergi uygulanır.
     public double tax() {
         return salary < 1000 ? 0.0 : salary * 0.03;
     }
 
+    // Bonus: Haftalık 40 saatin üzerindeki her saat için 30 TL bonus eklenir.
     public double bonus() {
         return workHours > 40 ? (workHours - 40) * 30 : 0.0;
     }
 
+
+    // raiseSalary(): ÇAlışma süresine göre zam hesaplar.
+    // 10 yıldan az: %5, 10-19 yıl arası: %10, 20 yıl ve üstü : %15
     public double raiseSalary() {
         int currentYear = 2021;
-        int yearWorked = currentYear - hireYear;
+        int yearsWorked = currentYear - hireYear;
 
-        if (yearWorked < 10) {
+        if (yearsWorked < 10) {
             return salary * 0.05;
-        } else if (yearWorked < 20) {
+        } else if (yearsWorked < 20) {
             return salary * 0.10;
         } else {
             return salary * 0.15;
         }
     }
 
+    // toString : Tüm çalışan bilgilerini ve hesapları yazdıran metot.
     public String toString() {
         double tax = tax();
         double bonus = bonus();
@@ -43,13 +64,12 @@ public class Employee {
 
         return "Adı: " + name +
                 "\nMaaşı: " + salary +
-                "\nÇalışma saati: " + workHours +
-                "\nBaşlangıç yılı: " + hireYear +
+                "\nÇalışma Saati: " + workHours +
+                "\nBaşlangıç Yılı: " + hireYear +
                 "\nVergi: " + tax +
                 "\nBonus: " + bonus +
-                "\nMaaş artışı: " + raise +
+                "\nMaaş Artışı: " + raise +
                 "\nVergi ve Bonuslar ile birlikte maaş: " + salaryWithTaxAndBonus +
                 "\nToplam Maaş: " + totalSalary;
     }
-
 }
